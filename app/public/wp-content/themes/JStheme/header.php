bdb8b74c8c630194859b6f1d74c3bcf8e0170995
Jayr Sullano <jarutosurano@gmail.com>
2023-11-01 11:57:50 -0400

    static menu but highlighting menu when active

--- app/public/wp-content/themes/JStheme/header.php
+++ app/public/wp-content/themes/JStheme/header.php
@@ -23,7 +23,7 @@
                             'theme_location' => 'headerMenuLocation'
                         ));
                         ?>
-                        <!-- <li><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
+                        <!-- <li <?php if (is_page('about-us') or wp_get_post_parent_id(0)) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
                         <li><a href="<?php echo site_url('/programs'); ?>">Programs</a></li>
                         <li><a href="<?php echo site_url('/events'); ?>">Events</a></li>
                         <li><a href="<?php echo site_url('/campuses'); ?>">Campuses</a></li>
